9ec273c3e1e0a0696e729d8115babb10b33af917
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-09-03 21:33:44 +0300

    хм

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,10 @@
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 console.log(galleryItems);
 
 
-// Change code below this line
 
 const gallery = document.querySelector('.gallery');
 
