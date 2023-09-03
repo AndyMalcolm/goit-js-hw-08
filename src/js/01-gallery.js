a2bdc35a50a40824ae343e7591d1284f00774fdd
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-09-03 18:06:46 +0300

    nice

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,16 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+document.addEventListener("DOMContentLoaded", () => {
+    const gallery = new SimpleLightbox(".gallery a", {
+    });
+  });
+  const gallery = new SimpleLightbox(".gallery a", {
+  });
+console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+window.addEventListener('load', () => {
+  const storedState = localStorage.getItem('feedback-form-state');
+  if (storedState) {
+    const { email, message } = JSON.parse(storedState);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+});
+
+const saveStateThrottled = throttle(() => {
+  const currentState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
+}, 500);
+
+emailInput.addEventListener('input', saveStateThrottled);
+messageInput.addEventListener('input', saveStateThrottled);
+
+feedbackForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+  console.log('Submitted data:', {
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+});
 
