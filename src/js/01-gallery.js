bc088af35f8a92828268958ab65cfbdff79f22ab
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-09-05 13:23:58 +0300

    найс

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,41 +1,57 @@
+// import "simplelightbox/dist/simple-lightbox.min.css";
+// import SimpleLightbox from "simplelightbox";
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+// console.log(galleryItems);
+
+
+
+//   const gallery = document.querySelector('.gallery');
+
+// function createGalleryItem(item) {
+//   const galleryItem = document.createElement('li');
+//   galleryItem.classList.add('gallery__item');
+
+//   const link = document.createElement('a');
+//   link.classList.add('gallery__link');
+//   link.href = item.original;
+
+//   const image = document.createElement('img');
+//   image.classList.add('gallery__image');
+//   image.src = item.preview;
+//   image.setAttribute('data-source', item.original);
+//   image.alt = item.description;
+
+//   link.appendChild(image);
+//   galleryItem.appendChild(link);
+
+//   return galleryItem;
+// }
+
+// const galleryItemsMarkup = galleryItems.map(createGalleryItem);
+// gallery.append(...galleryItemsMarkup);
+
+// const lightbox = new SimpleLightbox('.gallery a.gallery__link', {
+//   captionsData: "alt",
+//   captionDelay: 250,
+// });
+
+// const galleryHtml = `
+//   <ul class="gallery">
+//     ${galleryItems.map(item => `
+//       <li class="gallery__item">
+//         <a class="gallery__link" href="${item.original}">
+//           <img class="gallery__image" src="${item.preview}" alt="${item.description}" data-source="${item.original}" />
+//         </a>
+//       </li>
+//     `).join('')}
+//   </ul>
+// `;
 import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 console.log(galleryItems);
-
-
-
-  const gallery = document.querySelector('.gallery');
-
-function createGalleryItem(item) {
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
-
-  const link = document.createElement('a');
-  link.classList.add('gallery__link');
-  link.href = item.original;
-
-  const image = document.createElement('img');
-  image.classList.add('gallery__image');
-  image.src = item.preview;
-  image.setAttribute('data-source', item.original);
-  image.alt = item.description;
-
-  link.appendChild(image);
-  galleryItem.appendChild(link);
-
-  return galleryItem;
-}
-
-const galleryItemsMarkup = galleryItems.map(createGalleryItem);
-gallery.append(...galleryItemsMarkup);
-
-const lightbox = new SimpleLightbox('.gallery a.gallery__link', {
-  captionsData: "alt",
-  captionDelay: 250,
-});
 
 const galleryHtml = `
   <ul class="gallery">
@@ -48,3 +64,11 @@ const galleryHtml = `
     `).join('')}
   </ul>
 `;
+
+const gallery = document.querySelector('.gallery');
+gallery.innerHTML = galleryHtml;
+
+const lightbox = new SimpleLightbox('.gallery a.gallery__link', {
+  captionsData: "alt",
+  captionDelay: 250,
+});
