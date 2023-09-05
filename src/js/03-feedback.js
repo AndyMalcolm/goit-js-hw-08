55bf7952919cdeae0b06fb48aa03ebd5644b6d96
AndyMalcolm <ayhrunin.fitu21@kubg.edu.ua>
2023-09-05 14:25:14 +0300

    tak?

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,32 +4,32 @@ const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = feedbackForm.querySelector('input[name="email"]');
 const messageInput = feedbackForm.querySelector('textarea[name="message"]');
 
-window.addEventListener('load', () => {
+function loadFormDataFromLocalStorage() {
   const storedState = localStorage.getItem('feedback-form-state');
   if (storedState) {
     const { email, message } = JSON.parse(storedState);
     emailInput.value = email;
     messageInput.value = message;
   }
-});
+}
 
-const saveStateThrottled = throttle(() => {
+function saveFormDataToLocalStorage() {
   const currentState = {
     email: emailInput.value,
     message: messageInput.value,
   };
   localStorage.setItem('feedback-form-state', JSON.stringify(currentState));
-}, 500);
-
-emailInput.addEventListener('input', saveStateThrottled);
-messageInput.addEventListener('input', saveStateThrottled);
+}
+loadFormDataFromLocalStorage();
+emailInput.addEventListener('input', throttle(saveFormDataToLocalStorage, 500));
+messageInput.addEventListener('input', throttle(saveFormDataToLocalStorage, 500));
 
 feedbackForm.addEventListener('submit', (e) => {
   e.preventDefault();
   if (emailInput.value && messageInput.value) {
-    localStorage.removeItem('feedback-form-state');
     emailInput.value = '';
     messageInput.value = '';
+    localStorage.removeItem('feedback-form-state');
     console.log('Submitted data:', {
       email: emailInput.value,
       message: messageInput.value,
